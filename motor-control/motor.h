905f36097c5c707de996fa6b47cd4cbf2a8cb7d6
Mikaburrie <mikaburrie@gmail.com>
2024-05-06 06:32:02 -0900

    Add motor balancing

--- motor-control/motor.h
+++ motor-control/motor.h
@@ -3,31 +3,13 @@
 #include <stdlib.h>
 #include <math.h>
 
-// Distances in micrometers
-#define WHEELBASE 150000
-#define WHEEL_DIAMETER 62000
-#define TICKS_PER_ROTATION 20
 #define PI 3.14159
-#define WHEEL_CIRCUMFERENCE PI*WHEEL_DIAMETER
-#define DISTANCE_PER_TICK (WHEEL_CIRCUMFERENCE/TICKS_PER_ROTATION)
 
-// Time in microseconds
-#define MINIMUM_ENCODER_INTERVAL 5000UL
+// Encoder parameters (time in microseconds)
+#define TICKS_PER_ROTATION 20
+#define MINIMUM_ENCODER_INTERVAL 10000UL
 #define MAXIMUM_ENCODER_INTERVAL 200000UL
-
-#define ENCODER_P 2
-#define VELOCITY_BALANCE 0.5
-
-long long micros() {
-    struct timeval tv;
-    gettimeofday(&tv,NULL);
-    return 1000000*(long long)tv.tv_sec + tv.tv_usec;
-}
-
-void delay(int ms) {
-    long long start = micros();
-    while (micros() - start < ms*1000) {}
-}
+#define ENCODER_P 1
 
 // Struct containing motor settings and values
 struct Motor {
@@ -35,9 +17,10 @@ struct Motor {
     const int gpioPWM; // not to be confused with pigpio's gpioPWM() function
     const int gpioDir;
     const int gpioEncoder;
+    const int reversed;
     
-    int encoderTick;
-    uint32_t encoderTime;
+    volatile int encoderTick;
+    volatile uint32_t encoderTime;
 
     float velocityTarget;
     float velocityOutput;
@@ -57,7 +40,7 @@ void motorOnEncoderTick(struct Motor* m, int level, uint32_t now) {
 
     // Handle tick
     int8_t direction = (m->velocityOutput < 0 ? -1 : 1);
-    m->velocityMeasured = DISTANCE_PER_TICK/delta*direction;
+    m->velocityMeasured = 1000000.0/delta*direction;
     m->encoderTick += direction;
     m->encoderTime = now;
 }
@@ -96,19 +79,17 @@ void motorInitialize(struct Motor* m, gpioISRFunc_t onEncoderTick) {
     // Set default values
     m->encoderTick = 0;
     m->encoderTime = 0;
-
     m->velocityTarget = 0;
     m->velocityOutput = 0;
     m->velocityMeasured = 0;
     m->speedMax = 0;
-
     m->powerMin = 0;
     m->powerStart = 0;
 }
 
 void motorSetDuty(struct Motor* m, int duty) {
     // Set PWM and direction
-    int dir = duty < 0;
+    int dir = (duty < 0);
     gpioPWM(m->gpioPWM, duty*(dir ? -1 : 1));
     gpioWrite(m->gpioDir, dir);
 }
@@ -117,7 +98,7 @@ void motorTerminate(struct Motor* m) {
     // Clear motor output and encoder interrupt
     gpioPWM(m->gpioPWM, 0);
     gpioWrite(m->gpioPWM, 0);
-    gpioSetISRFunc(m->gpioEncoder, EITHER_EDGE, 0, NULL);
+    gpioSetISRFunc(m->gpioEncoder, FALLING_EDGE, 0, NULL);
 }
 
 // Calculates powerStart, powerMin, and speedMax for a motor
@@ -132,38 +113,39 @@ void motorCalibrate(struct Motor* m) {
             break;
         }
         motorSetDuty(m, power);
-        delay(16);
+        usleep(16000);
     }
 
     // Drive at full power and measure speed
     motorSetDuty(m, 200);
-    delay(1000);
+    usleep(900000);
 
     float speed = 0;
     for (uint8_t i = 0; i < 64; i++) {
         speed += m->velocityMeasured;
-        delay(16);
+        usleep(16000);
     }
 
     m->speedMax = speed/64;
 
     // Decrease power until motor stops and store result
     for (uint8_t power = m->powerStart; power > 0; power--) {
-        if ((micros() - m->encoderTime) > MAXIMUM_ENCODER_INTERVAL) {
+        if (gpioTick() - m->encoderTime > MAXIMUM_ENCODER_INTERVAL) {
             m->powerMin = power + 10;
             break;
         }
         motorSetDuty(m, --power);
-        delay(50);
+        usleep(50000);
     }
 
+    motorSetDuty(m, 0);
     m->velocityTarget = 0;
 }
 
 // Determines motor speed and adjusts output to match target
 void motorUpdate(struct Motor* m, float delta) {
     // Assume motor stopped if no encoder tick occurs in a certain interval
-    if ((micros() - m->encoderTime) > MAXIMUM_ENCODER_INTERVAL) m->velocityMeasured = 0;
+    if ((gpioTick() - m->encoderTime) > MAXIMUM_ENCODER_INTERVAL) m->velocityMeasured = 0;
 
     // Update PID loop (really just P)
     float error = m->velocityTarget - m->velocityMeasured;
@@ -174,18 +156,18 @@ void motorUpdate(struct Motor* m, float delta) {
     m->velocityOutput = fmax(-m->speedMax, fmin(m->velocityOutput, m->speedMax));
 
     // Calculate power output
-    uint16_t power = (uint16_t) 255*abs(m->velocityOutput)/m->speedMax; // Speed -> power
-    if (m->powerMin < power && power < m->powerStart && abs(m->velocityMeasured) < 0.01) power = m->powerStart; // Start push
-    power = fmin(power, 255)*(power > m->powerMin); // Range
+    int16_t power = (int16_t) 255*m->velocityOutput/m->speedMax; // Speed -> power
+    if (m->powerMin < abs(power) && abs(power) < m->powerStart && m->velocityMeasured == 0) power = m->powerStart*(power < 0 ? -1 : 1); // Start push
 
     // Set motor power
+    printf("%f %d\n",  m->velocityOutput, power);
     motorSetDuty(m, power);
 }
 
 // Sets the motor speed
 void motorSetVelocity(struct Motor* m, float velocity) {
-    float speed = abs(velocity);
-
+    float speed = fabs(velocity);
+    
     // Zero power if below minimum
     if (speed < (m->speedMax*m->powerMin/255)) {
         m->velocityTarget = 0;
--- motor-control/motor_control.c
+++ motor-control/motor_control.c
@@ -7,6 +7,7 @@
 #define IPC_BUFFER_SIZE 20
 
 #include "motor.h"
+#define VELOCITY_BALANCE 0.5
 
 // Define GPIO pins for motor output and encoder input
 #define GPIO_MOTOR_LEFT_PWM 19
@@ -21,18 +22,21 @@
 struct Motor leftMotor = {
     GPIO_MOTOR_LEFT_PWM,
     GPIO_MOTOR_LEFT_DIR,
-    GPIO_ENCODER_LEFT
+    GPIO_ENCODER_LEFT,
+    0
 };
 
 struct Motor rightMotor = {
     GPIO_MOTOR_RIGHT_PWM,
     GPIO_MOTOR_RIGHT_DIR,
-    GPIO_ENCODER_RIGHT
+    GPIO_ENCODER_RIGHT,
+    1 // reversed
 };
 
 // Define encoder tick handlers
 void onLeftEncoderTick(int gpio, int level, uint32_t time) {
 //    printf("Left encoder tick: level %d at %d us\n", level, time);
+    if (level) return;
     motorOnEncoderTick(&leftMotor, level, time);
 }
 
@@ -55,9 +59,16 @@ int main(int argc, char *argv[]) {
     motorInitialize(&leftMotor, onLeftEncoderTick);
     motorInitialize(&rightMotor, onRightEncoderTick);
     
-    // Calibraqte
+    // Calibrate
     motorCalibrate(&leftMotor);
-    printf("Min power: %d, Start power: %d, Max speed: %f\n", leftMotor.powerMin, leftMotor.powerStart, leftMotor.speedMax);
+    motorCalibrate(&rightMotor);
+    printf("Calibration result:\nMin power: %d, Start power: %d, Max speed: %f\n", leftMotor.powerMin, leftMotor.powerStart, leftMotor.speedMax);
+    printf("Calibration result:\nMin power: %d, Start power: %d, Max speed: %f\n", rightMotor.powerMin, rightMotor.powerStart, rightMotor.speedMax);
+    
+    // Set left and right speedMax to minimum
+    float speedMax = fmin(leftMotor.speedMax, rightMotor.speedMax);
+    leftMotor.speedMax = speedMax;
+    rightMotor.speedMax = speedMax;
     
     // Create and open non-blocking FIFO pipe for receiving motor commands
     int fileDescriptor;
@@ -68,7 +79,7 @@ int main(int argc, char *argv[]) {
     printf("File: %d\n", fileDescriptor);
     
     // Define timing variables
-    float now = micros();
+    float now = gpioTick();
     float lastUpdate = now;
     
     // Loop until program is terminated
@@ -78,16 +89,29 @@ int main(int argc, char *argv[]) {
         
         // Set left and right motor speeds if command is received
         if (bytesReceived > 0) {
-            motorSetVelocity(&leftMotor, dataBuffer[0]);
-        //    motorSetDuty(&rightMotor, dataBuffer[1]);
+            motorSetVelocity(&leftMotor, leftMotor.speedMax*dataBuffer[0]/100);
+            motorSetVelocity(&rightMotor, -rightMotor.speedMax*dataBuffer[1]/100);
         }
         
-        now = micros();
+        // Calculate delta time
+        now = gpioTick();
         float delta = (now - lastUpdate)/1000000.0;
         
+        // Adjust speeds to maintain proper ratio
+        if (leftMotor.velocityTarget != 0 && rightMotor.velocityTarget != 0) {
+            float ratio = leftMotor.velocityTarget/rightMotor.velocityTarget;
+            float leftSpeedError = rightMotor.velocityMeasured*ratio - leftMotor.velocityMeasured;
+            float rightSpeedError = leftMotor.velocityMeasured/ratio - rightMotor.velocityMeasured;
+            leftMotor.velocityOutput += leftSpeedError*VELOCITY_BALANCE*delta;
+            rightMotor.velocityOutput -= rightSpeedError*VELOCITY_BALANCE*delta;
+        }
+        
+        // Update motors
         motorUpdate(&leftMotor, delta);
+        motorUpdate(&rightMotor, delta);
         
         lastUpdate = now;
+        usleep(10000);
     }
     
     // Close and remove FIFO pipe
